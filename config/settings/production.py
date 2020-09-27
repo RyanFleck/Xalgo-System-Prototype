356faf3f5bb3fbc33bad726fa352a129ef403a15
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-09-27 17:56:32 -0600

    Update allowed host in production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["xalgorithms.org"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["xalgo-system.herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
