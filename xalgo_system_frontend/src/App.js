b8ca10fdb329801b28a3d7c3871ab9fa13a1fc3c
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-10-24 14:53:05 -0600

    Use axios to get user info from react app

--- xalgo_system_frontend/src/App.js
+++ xalgo_system_frontend/src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
+
+let rules = null;
+let user = null;
 
 function App() {
+  const [username, setUsername] = useState("Anonymous");
+  const [user, setUser] = useState(null);
+  const [rules, setRules] = useState(null);
+
+  useEffect(() => {
+    Axios.get("/rest-auth/user")
+      .then((res) => {
+        setUser(res.data);
+        setUsername(res.data.username);
+        console.log(`User ${res.data.username} is authenticated.`);
+      })
+      .catch((err) => {
+        const status = err.response.status;
+        if (status === 403) {
+          console.log(`Failed to authenticate user: ${status}`);
+          setUsername("Unauthenticated");
+        } else {
+          console.log(`Error while getting user info: ${status}`);
+        }
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, {username}.</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
