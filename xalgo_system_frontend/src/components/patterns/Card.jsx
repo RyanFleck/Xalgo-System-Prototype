3d493c21fe6f8838a202a0aa6d33f69a22c9fc27
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-10-25 11:29:09 -0600

    Load user rules to dashboard.

--- xalgo_system_frontend/src/components/patterns/Card.jsx
+++ xalgo_system_frontend/src/components/patterns/Card.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Text, Flex, Icon, Box, Button } from '..';
+import { Link } from '@reach/router';
 
-export default function Card() {
+export default function Card({ name, uuid, downloadLink, editLink }) {
   return (
-    <Box
-      p={2}
-      bg="bg"
-      border="1px solid"
-      borderColor="oline"
-      borderRadius="base"
-      marginBottom={3}
-    >
+    <Box p={2} bg="bg" border="1px solid" borderColor="oline" borderRadius="base" marginBottom={3}>
       <Flex alignItems="flex-start">
-        <Text>Loi concernant la taxe sur les carburants</Text>
+        <Text>{name || 'Rule Name'}</Text>
       </Flex>
-      <Box padding={2} />
+      <Flex alignItems="flex-start">
+        <Text color="primary">
+          <code>
+            <small>{uuid || '0000-0000-0000-0000'}</small>
+          </code>
+        </Text>
+      </Flex>
+      <Box padding={1} />
       <Flex justifyContent="space-between">
         <Button variant="invisible">
-          <Flex alignItems="center">
-            <Icon name="download" />
-            <Text color="primary">Download</Text>
-          </Flex>
+          <Link to={downloadLink || ''}>
+            <Flex alignItems="center">
+              <Icon name="download" />
+              <Text color="primary">Download</Text>
+            </Flex>
+          </Link>
         </Button>
         <Button variant="invisible">
-          <Text color="primary">Edit Rule</Text>
+          <Link to={editLink || ''}>
+            <Text color="primary">Edit Rule</Text>
+          </Link>
         </Button>
       </Flex>
     </Box>
--- xalgo_system_frontend/src/layouts/Dashboard.jsx
+++ xalgo_system_frontend/src/layouts/Dashboard.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 import Box from '../components/layout/Box';
 import Grid from '../components/layout/Grid';
 import Card from '../components/patterns/Card';
+import Flex from '../components/layout/Flex';
+
 // rm-components
 import Text from '../components/primitives/Text';
 import ScrollUp from './components/ScrollUp';
+import { Link } from '@reach/router';
+import Axios from 'axios';
+import BarLoader from 'react-spinners/BarLoader';
 
 const hold = {
   zIndex: '5',
@@ -17,7 +22,25 @@ export default class Dashboard extends React.Component {
     super(props);
     this.state = {
       pageDescription: 'Rule Maker Dashboard',
+      rules: [],
+      ready: false,
     };
+  }
+
+  componentDidMount() {
+    Axios.get('/rules/rule')
+      .then((res) => {
+        this.setState({ rules: res.data, ready: true });
+        console.log(res.data);
+      })
+      .catch((err) => {
+        const status = err.response.status;
+        if (status === 403) {
+          console.log(`Failed to authenticate user: ${status}`);
+        } else {
+          console.log(`Error while getting user info: ${status}`);
+        }
+      });
   }
 
   render() {
@@ -32,23 +55,44 @@ export default class Dashboard extends React.Component {
                     <Text variant="formtitle">Editor</Text>
                     <Box p={2} />
                     <Box>
-                      <Text color="primary">Create Rule</Text>
+                      <Link to="">
+                        <Text color="primary">Create Rule</Text>
+                      </Link>
                     </Box>
                     <Box marginTop={2}>
                       <Text color="textb">Create Table</Text>
                     </Box>
                   </Box>
                 </Box>
-                <Box p={4}  borderRight="1px solid #efefef">
+                <Box p={4} borderRight="1px solid #efefef">
                   <Box p={1} />
                   <Text>Graphics showing rule ussage will go here in the future</Text>
                 </Box>
                 <Box p={4} height="auto">
                   <Text variant="formtitle">My Rules</Text>
                   <Box p={2} />
-                  <Card />
-                  <Card />
-                  <Card />
+                  {this.state.ready ? (
+                    <div>
+                      {this.state.rules.map((e, i) => {
+                        console.log(e);
+                        return (
+                          <Card
+                            key={i}
+                            uuid={e.id}
+                            name={e.name}
+                            downloadLink=""
+                            editLink={`/apps/rm/editor/${e.id}`}
+                          />
+                        );
+                      })}
+                    </div>
+                  ) : (
+                    <div>
+                      <Flex alignitems="center">
+                        <BarLoader width={200} />
+                      </Flex>
+                    </div>
+                  )}
                 </Box>
               </Grid>
             </Box>
