316980b9dd8224a87f5119463e26c88547636296
Ryan Fleck <Ryan.Fleck@protonmail.com>
2020-11-10 18:41:20 -0700

    Bump packages.

--- xalgo_system_frontend/src/components/patterns/Search.jsx
+++ xalgo_system_frontend/src/components/patterns/Search.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Input, Flex, Icon, Box, Button } from '..';
 
-export default () => (
-  <Box m={0} width={1} bg="bg">
-    <Flex justifyContent="space-between" alignItems="center">
-      <Button variant="invisible">
-        <Flex alignItems="center" marginTop="4px">
-          <Icon name="search" fill="#000" />
-        </Flex>
-      </Button>
-      <Input variant="search" type="text" m={2} />
-    </Flex>
-  </Box>
-);
+function Search() {
+  return (
+    <Box m={0} width={1} bg="bg">
+      <Flex justifyContent="space-between" alignItems="center">
+        <Button variant="invisible">
+          <Flex alignItems="center" marginTop="4px">
+            <Icon name="search" fill="#000" />
+          </Flex>
+        </Button>
+        <Input variant="search" type="text" m={2} />
+      </Flex>
+    </Box>
+  );
+}
+
+export default Search;
