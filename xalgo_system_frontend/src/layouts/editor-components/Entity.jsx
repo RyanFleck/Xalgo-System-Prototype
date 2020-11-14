16973a43617614322c683d53523418202f935691
Calvin Hutcheon <chutcheon@mica.edu>
2020-11-14 01:12:03 -0800

    adding untracked rule maker entity file

---
+++ xalgo_system_frontend/src/layouts/editor-components/Entity.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { RuleSchema } from 'xalgo-rule-processor';
+import { Box, FormStandard, IdDisplay, Text } from '../../components';
+
+function Entity({ rule, updateRule, active }) {
+  // 0. Fill out the section name.
+  const sectionName = 'Rule Maintainer';
+  //const sectionDesc = 'Begin your rule by providing a title and concise description.';
+  const [modified, setModified] = useState(false);
+
+  // 1. Set a state for each element that must be filled.
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  // Don't touch this.
+  if (active && !modified) {
+    console.log(`${sectionName} section is being edited.`);
+
+    // 2. Ensure each field is set according to the current rule state.
+    if (name !== rule.metadata.rule.maintainers[0].name) setName(rule.metadata.rule.maintainers[0].name);
+    if (email !== rule.metadata.rule.maintainers[0].email) setEmail(rule.metadata.rule.maintainers[0].email);
+  }
+
+  
+  /*function saveContent() {
+    console.log(`Saving ${sectionName} to state.`);
+    rule.metadata.rule.maintainers[0].name = name;
+    rule.metadata.rule.maintainers[0].email = email;
+    updateRule(rule);
+    setModified(false);
+  }*/
+
+
+
+
+
+  return (
+    <div>
+      <Box border="1px solid" borderColor="oline" borderRadius="base" p={3} bg="#fff">
+        <Text variant="formtitle">Rule Maker Entity</Text>
+        <Box p={1} />
+        <IdDisplay message="Vqp4nv8eGprI" />
+              <Box padding={1} />
+              <FormStandard 
+                name="Rule Maker Entity Name" 
+                description={RuleSchema.metadata.rule.maintainers[0].__name}
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                  setModified(true);
+                }} 
+              />
+              <Box padding={1} />
+              <FormStandard name="Rule Maker Entity URL" 
+                description={RuleSchema.metadata.rule.maintainers[0].__email}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setModified(true);
+                }}  
+              />
+      </Box>
+    </div>
+  );
+}
+
+export default Entity;
